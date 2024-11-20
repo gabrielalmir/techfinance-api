7c4b1cb8e9fd525fea5db4ad5da6f392567bdd8c
gabrielalmir <gabr.almir@gmail.com>
2024-11-20 08:53:01 -0300

    feat: remove sql import from db/index.ts

--- app/src/db/index.ts
+++ app/src/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { env } from '../config/env';
 import * as schema from './schema';
-import { sql } from 'drizzle-orm';
 
 export const db = drizzle(env.DATABASE_URL, { schema });
