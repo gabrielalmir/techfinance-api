1270d30c1f17a1829248d8b170b8201d5bc06e79
gabrielalmir <gabr.almir@gmail.com>
2024-11-24 17:37:49 -0300

    feat: specify date pattern

--- app/src/services/payment.service.ts
+++ app/src/services/payment.service.ts
@@ -16,7 +16,7 @@ export class PaymentService {
 
         const result = await this.paymentRepository.getSummaryAIData();
         const serializedPaymentResult = JSON.stringify(result);
-        const promptMessage = `Data de Hoje = ${new Date().toLocaleString('pt-br')}\n\n${prompt}:\n\n${serializedPaymentResult}. Até 500 caracteres.\n\n`;
+        const promptMessage = `Data de Hoje = ${new Date().toLocaleString('pt-br')}\n\n${prompt}:\n\n${serializedPaymentResult}.\n\n`;
         const response = await this.promptService.generateResponse(promptMessage);
 
         return response;
