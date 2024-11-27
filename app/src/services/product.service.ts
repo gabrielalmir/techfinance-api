e98a8988b07c31961b5ebf4d4f1281d5618a94a5
gabrielalmir <gabr.almir@gmail.com>
2024-11-27 20:51:31 -0300

    feat: change description to name in products

--- app/src/services/product.service.ts
+++ app/src/services/product.service.ts
@@ -4,7 +4,7 @@ import { db } from "../db";
 import { produtos } from "../db/schema";
 
 const productQuerySchema = z.object({
-    descricao: z.string().optional(),
+    nome: z.string().optional(),
     grupo: z.string().optional(),
     limite: z.coerce.number().optional(),
     pagina: z.coerce.number().optional(),
@@ -12,12 +12,12 @@ const productQuerySchema = z.object({
 
 export class ProductService {
     async getProducts(query: Record<string, string | undefined>) {
-        const { descricao = '', grupo = '', limite = 10, pagina = 1 } = productQuerySchema.parse(query);
+        const { nome = '', grupo = '', limite = 10, pagina = 1 } = productQuerySchema.parse(query);
         const offset = (pagina - 1) * limite;
 
         const products = await db.select()
             .from(produtos)
-            .where(and(ilike(produtos.descricao_produto, `%${descricao}%`), ilike(produtos.descricao_grupo, `%${grupo}%`)))
+            .where(and(ilike(produtos.descricao_produto, `%${nome}%`), ilike(produtos.descricao_grupo, `%${grupo}%`)))
             .limit(limite)
             .offset(offset)
             .execute();
