c260ca90f2a09fc1cfe7de376a04dc9cd5108ce1
gabrielalmir <gabr.almir@gmail.com>
2025-04-02 19:56:34 -0300

    feat: adicionar validação de consulta para a rota de clientes

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -1,10 +1,23 @@
 import { Elysia } from 'elysia';
+import { z } from 'zod';
 import { customerService } from '../config/deps';
+
+const customerQuerySchema = z.object({
+    nome: z.string().optional(),
+    grupo: z.string().optional(),
+    limite: z.coerce.number().optional(),
+    pagina: z.coerce.number().optional(),
+});
 
 export const customerRoutes = (app: Elysia) => {
     app.get('/clientes', async ({ query }) => {
         try {
-            return await customerService.getCustomers(query);
+            const parsedQuery = customerQuerySchema.parse(query);
+
+            const { nome = '', grupo = '', limite = 10, pagina = 1 } = parsedQuery;
+            const offset = (pagina - 1) * limite;
+
+            return await customerService.getCustomers({ name: nome, group: grupo, limit: limite, page: pagina, offset });
         } catch (error) {
             return { status: 500, message: 'Erro ao obter clientes' };
         }
--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,23 +1,12 @@
-import { z } from "zod";
-import type { CustomerRepository } from "../repositories/customer.repository";
-
-const customerQuerySchema = z.object({
-    nome: z.string().optional(),
-    grupo: z.string().optional(),
-    limite: z.coerce.number().optional(),
-    pagina: z.coerce.number().optional(),
-});
+import type { CustomerRepository, GetCustomersQuery } from "../repositories/customer.repository";
 
 export class CustomerService {
     constructor(
         private readonly customerRepository: CustomerRepository,
     ) { }
 
-    async getCustomers(query: Record<string, string | undefined>) {
-        const { nome = '', grupo = '', limite = 10, pagina = 1 } = customerQuerySchema.parse(query);
-        const offset = (pagina - 1) * limite;
-        const customers = await this.customerRepository.getCustomers({ name: nome, group: grupo, limit: limite, page: pagina, offset });
-
+    async getCustomers(query: GetCustomersQuery) {
+        const customers = await this.customerRepository.getCustomers(query);
         return customers;
     }
 }
